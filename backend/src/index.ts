9a8cd58f863df79328f8d2aec8ada5407a7d3b74
KhalandarMokula <khalandar.mokula@gmail.com>
2024-04-27 19:45:52 +0530

    Fix Error in routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,14 +22,21 @@ app.put('/api/v1/blog', (c)=> {
   return c.text("post edited");
 })
 
+/*
+The issue seems to be that both /api/v1/blog/:id and /api/v1/blog/bulk are being handled by the same route, 
+and it's likely that the route for /api/v1/blog/:id is being matched first, causing /api/v1/blog/bulk to be redirected to /api/v1/blog/:id.
+To resolve this issue, you can reorder the route definitions so that more specific routes are defined before more general ones.
+*/
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text("bulk blog");
+})
+
 app.get('/api/v1/blog/:id', (c)=> {
   const id = c.req.param('id');
   
   return c.text("request for blog with if: {id} received");
 })
 
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text("bulk blog");
-})
+
 
 export default app
